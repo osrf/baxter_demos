904acfb90c83c1f89712de02f1c8177e4cd94bd2
Jackie Kay <jackie@osrfoundation.org>
2014-08-01 17:50:41 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup()
-d['packages'] = ['baxter_demos']
-d['package_dir'] = {'': 'scripts'}
+#d['packages'] = ['baxter_demos']
+#d['package_dir'] = {'': 'scripts'}
 
 setup(**d)
