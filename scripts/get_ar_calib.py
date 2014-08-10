d513660b89591b4b468872a8d41131fb249dd8cd
Jackie Kay <jackie@osrfoundation.org>
2014-08-10 12:52:19 -0700

    fixed calibration

--- scripts/get_ar_calib.py
+++ scripts/get_ar_calib.py
@@ -8,16 +8,26 @@ from math import pi
 from geometry_msgs.msg import Pose, Point, Quaternion
 from visualization_msgs.msg import Marker, MarkerArray
 import visualization_msgs.msg
+#from ar_track_alvar import AlvarMarkers
 
 # TODO: add averaging/filtering, auto-add to param server or urdf or file
 
-markernum = '2'
-measured_translation = [0.06991+0.01036+0.0225, -0.0569, -0.0055]
-forearm_marker_rot = numpy.array( [[-1, 0, 0], [0, 0, -1], [0, -1, 0]] )
+class markerSubscriber():
+    def __init__(self, markernum=2):
+        self.sub = rospy.Subscriber("/visualization_marker", Marker, self.callback)
+        self.markernum = markernum
+        self.pose = None
+    def callback(self, data):
+        self.pose = data.pose
 
 def getPoseFromMatrix(matrix):
     trans, quat = getTfFromMatrix(numpy.linalg.inv(matrix))
     return Pose(position=Point(*trans), orientation=Quaternion(*quat))
+
+def getMatrixFromPose(pose):
+    trans = (pose.position.x, pose.position.y, pose.position.z )
+    rot = tf.transformations.euler_from_quaternion((pose.orientation.x, pose.orientation.y, pose.orientation.z, pose.orientation.w))
+    return tf.transformations.compose_matrix(translate = trans, angles = rot)
 
 def getTfFromMatrix(matrix):
     scale, shear, angles, trans, persp = tf.transformations.decompose_matrix(matrix)
@@ -46,6 +56,10 @@ def create_marker(ns, id_num, shape_type, pose, color, scale):
     marker.scale.x, marker.scale.y, marker.scale.z = scale
     return marker
 
+markernum = '2'
+measured_translation = [0.06991+0.01036+0.0225, -0.0569, -0.0055]
+forearm_marker_rot = numpy.array( [[-1, 0, 0], [0, 0, -1], [0, -1, 0]] )
+
 rospy.init_node("get_ar_calib")
 
 tf_listener = tf.TransformListener()
@@ -55,15 +69,15 @@ marker_pub = rospy.Publisher("ar_calib_markers", MarkerArray)
 
 rate = rospy.Rate(100)
 
-
-#forearm_marker_trans = numpy.dot(numpy.linalg.inv(forearm_marker_rot),
-#                               -numpy.array(measured_translation).reshape((3, 1))).flatten()
-forearm_marker_trans = numpy.array(measured_translation)
+forearm_marker_trans = numpy.dot(numpy.linalg.inv(forearm_marker_rot),
+                               -numpy.array(measured_translation).reshape((3, 1))).flatten()
+#forearm_marker_trans = numpy.array(measured_translation)
 print forearm_marker_trans
 # Calculate transform from forearm to marker
 forearm_marker = tf.transformations.compose_matrix(
                 translate = forearm_marker_trans,
                 angles = tf.transformations.euler_from_matrix(forearm_marker_rot))
+marker_sub = markerSubscriber()
 
 # Publish transform and marker
 while not rospy.is_shutdown():
@@ -76,7 +90,6 @@ while not rospy.is_shutdown():
     base_marker = forearm_marker.dot(base_forearm)
     trans, rot = getTfFromMatrix(numpy.linalg.inv(base_marker))
     tf_broadcaster.sendTransform(trans, rot, rospy.Time.now(), "/ar_marker_2", "/base")
-    #tf_broadcaster.sendTransform(trans, rot, rospy.Time.now(), "/base", "/ar_marker_2")
     marker_pose = getPoseFromMatrix(base_marker)
 
     # marker to camera
@@ -87,14 +100,18 @@ while not rospy.is_shutdown():
     trans, rot = getTfFromMatrix(numpy.linalg.inv(base_camera))
 
     tf_broadcaster.sendTransform(trans, rot, rospy.Time.now(), "/camera_link", "/base")
-    #tf_broadcaster.sendTransform(trans, rot, rospy.Time.now(), "/base", "/camera_link")
     camera_pose = getPoseFromMatrix(base_camera)
-
-    marker_msg = create_marker("marker_pose", 44, Marker.CUBE, marker_pose, (0, 255, 0), (0.07, 0.04, 0.02) )
+    
+    squaredims = (0.07, 0.04, 0.02)
+    marker_msg = create_marker("marker_pose", 44, Marker.CUBE, marker_pose, (0, 255, 0), squaredims )
 
     camera_msg = create_marker("camera_pose", 1337, Marker.ARROW, camera_pose, (0, 0, 255), (0.2, 0.01, 0.01))
 
+    # Now refine the transform using least squares
+    # Get the pose of the marker from ar_pose_marker in the camera frame
     msg = [marker_msg, camera_msg]
+
     marker_pub.publish(msg)
     rate.sleep()
+
 
