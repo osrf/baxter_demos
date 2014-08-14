01c099c0012e32ef41a3a9f3eee2b4645213b2f7
Jackie Kay <jackie@osrfoundation.org>
2014-08-13 18:15:44 -0700

    Finding some objects in 3d

--- src/object_finder_3d.cpp
+++ src/object_finder_3d.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
-#include <fstream>
-#include "yaml-cpp/yaml.h"
+#include <exception>
+#include <cmath>
 
 #include "ros/ros.h"
 #include "tf/transform_listener.h"
@@ -21,22 +21,25 @@
 #include <pcl/search/search.h>
 #include <pcl/search/kdtree.h>
 #include <pcl/visualization/cloud_viewer.h>
+#include <pcl/features/moment_of_inertia_estimation.h>
 
 #include <pcl/conversions.h>
 #include <pcl/PCLPointCloud2.h>
 #include <pcl_conversions/pcl_conversions.h>
 
-using namespace std; //I don't like using multiple namespaces okaaay
+using namespace std;
 
-bool isPointWithinDesiredRange(const pcl::PointRGB input_pt, const pcl::PointRGB desired_pt, int radius){
-    // unsigned int subtraction--overflow
-    pcl::PointXYZRGB input_xyz(input_pt.r, input_pt.g, input_pt.b), desired_xyz(desired_pt.r, desired_pt.g, desired_pt.b);
+bool isPointWithinDesiredRange(const pcl::PointRGB input_pt,
+                               const pcl::PointRGB desired_pt, int radius){
+    pcl::PointXYZRGB input_xyz(input_pt.r, input_pt.g, input_pt.b),
+                        desired_xyz(desired_pt.r, desired_pt.g, desired_pt.b);
     pcl::PointXYZHSV input_hsv, desired_hsv;
     pcl::PointXYZRGBtoXYZHSV(input_xyz, input_hsv);
     pcl::PointXYZRGBtoXYZHSV(desired_xyz, desired_hsv);
     
-    if ( abs((int)input_hsv.h - (int)desired_hsv.h) < radius &&
-         abs((int)input_hsv.s - (int)desired_hsv.s) < radius && abs((int)input_hsv.v - (int)desired_hsv.v) < radius){
+    if ( abs(input_hsv.h - desired_hsv.h) < radius &&
+         abs(input_hsv.s - desired_hsv.s) < radius &&
+         abs(input_hsv.v - desired_hsv.v) < radius){
         return true;
     }
     return false;
@@ -56,6 +59,7 @@ private:
     bool has_cloud;
     pcl::PointRGB desired_color;
     
+    pcl::IndicesPtr indices;
     ros::NodeHandle n;
     ros::Subscriber sub;
     ros::Publisher pose_pub;
@@ -69,7 +73,6 @@ private:
 public:
 
     pcl::visualization::CloudViewer cloud_viewer;
-    //pcl::visualization::CloudViewer cluster_viewer;
     
     bool hasCloud(){
         return has_cloud;
@@ -79,51 +82,41 @@ public:
         return has_desired_color;
     }
 
-    CloudSegmenter() : cloud_viewer("Cloud viewer"){
-        string folder_name;
-        if(!n.getParam("/object_tracker/config_folder", folder_name)){
-            // probably freak out here
+    CloudSegmenter() : cloud_viewer("Cloud viewer"), indices( new vector<int>()){
+        map<string, int> params;
+        if(!n.getParam("/object_finder_params", params)){
+            throw runtime_error("Couldn't find parameters\n");
         }
-        //YAML::Node doc = YAML::LoadFile(folder_name);
-        ifstream fin(folder_name.c_str());
-        YAML::Parser parser(fin);
-        YAML::Node doc;
-        parser.GetNextDocument(doc);
-
-        doc["radius"] >> radius;
-        doc["filter_min"] >> filter_min;
-        doc["filter_max"] >> filter_max;
-        doc["distance_threshold"] >> distance_threshold;
-        doc["point_color_threshold"] >> point_color_threshold;
-        doc["region_color_threshold"] >> region_color_threshold;
-        doc["min_cluster_size"] >> min_cluster_size;
+        radius = params["radius"];
+        filter_min = params["filter_min"];
+        filter_max = params["filter_max"];
+        distance_threshold = params["distance_threshold"];
+        point_color_threshold = params["point_color_threshold"];
+        region_color_threshold = params["region_color_threshold"];
+        min_cluster_size = params["min_cluster_size"];
 
         has_desired_color = false;
         has_cloud = false;
-        //cloud_viewer.registerMouseCallback(&CloudSegmenter::getClickedPoint, *this, (void*) NULL);
-        cloud_viewer.registerPointPickingCallback(&CloudSegmenter::getClickedPoint, *this, (void*) NULL );
+        cloud_viewer.registerPointPickingCallback(
+                    &CloudSegmenter::getClickedPoint, *this, (void*) NULL );
 
         sub = n.subscribe("/camera/depth_registered/points", 500,
                                           &CloudSegmenter::callback, this);
-        //TODO: add args for topic name
-        pose_pub = n.advertise<geometry_msgs::PoseArray>("/object_tracker/right/goal_poses", 1000);
 
-        
+        //TODO: add args for topic name
+        pose_pub = n.advertise<geometry_msgs::PoseArray>(
+                            "/object_tracker/right/goal_poses", 1000);
     }
+
     void publish_poses(){
         geometry_msgs::PoseArray msg;
-        msg.poses = object_poses; //Does this assignment work? We'll see!!
+        msg.poses = object_poses;
         pose_pub.publish(msg);
     }
 
     //remember to shift-click!
-    void getClickedPoint(const pcl::visualization::PointPickingEvent& event, void* args){
-
-        //pcl::PointXYZ clicked_point;
-        //event.getPoint(clicked_point.x, clicked_point.y, clicked_point.z);
-        //cout << "Got point: " << clicked_point.x << ", " << clicked_point.y << ", " << clicked_point.z <<endl;
-        //desired_color = getCloudColorAt(clicked_point.x, clicked_point.y);
-
+    void getClickedPoint(const pcl::visualization::PointPickingEvent& event,
+                         void* args){
         int n = event.getPointIndex();
         if (n == -1){
             cout << "Got no point" << endl;
@@ -131,13 +124,14 @@ public:
         }
 
         desired_color = getCloudColorAt(n);
-        cout << "Desired color: " << (int) desired_color.r << ", " << (int) desired_color.g << ", " << (int) desired_color.b << endl;
+        cout << "Desired color: " << (int) desired_color.r << ", " <<
+                (int) desired_color.g << ", " << (int) desired_color.b << endl;
         has_desired_color = true;
         segmentation();
     }
 
     void renderCloud(){
-        cloud_viewer.showCloud(cloud); //Is this okay in this callback...?
+        cloud_viewer.showCloud(cloud);
     }
 
     void renderClusters(){
@@ -145,12 +139,12 @@ public:
     }
 
     pcl::PointRGB getCloudColorAt(int x, int y){
-        pcl::PointXYZRGB cur = cloud->at(x, y); //Indexing might be dubious
-        return pcl::PointRGB(cur.r, cur.g, cur.b);
+        pcl::PointXYZRGB cur = cloud->at(x, y);
+        return pcl::PointRGB(cur.b, cur.g, cur.r);
     }
     pcl::PointRGB getCloudColorAt(int n){
-        pcl::PointXYZRGB cur = cloud->at(n); //Indexing might be dubious
-        return pcl::PointRGB(cur.r, cur.g, cur.b);
+        pcl::PointXYZRGB cur = cloud->at(n);
+        return pcl::PointRGB(cur.b, cur.g, cur.r);
     }
 
    
@@ -166,21 +160,13 @@ public:
                             <pcl::PointXYZRGB> >
                             (new pcl::search::KdTree<pcl::PointXYZRGB>);
 
-        pcl::IndicesPtr indices (new vector <int>);
-        pcl::PassThrough<pcl::PointXYZRGB> pass;
-        pass.setInputCloud (cloud);
-        pass.setFilterFieldName ("z");
-        pass.setFilterLimits (0.0, 4.0);
-        pass.filter (*indices);
-        //TODO: yaml these parameters 
-
         reg.setInputCloud (cloud);
         reg.setIndices (indices);
         reg.setSearchMethod (tree);
-        reg.setDistanceThreshold (10);
-        reg.setPointColorThreshold (6);
-        reg.setRegionColorThreshold (5);
-        reg.setMinClusterSize (600);
+        reg.setDistanceThreshold (distance_threshold);
+        reg.setPointColorThreshold (point_color_threshold);
+        reg.setRegionColorThreshold (region_color_threshold);
+        reg.setMinClusterSize (min_cluster_size);
      
         vector <pcl::PointIndices> clusters;
         reg.extract (clusters);
@@ -189,27 +175,25 @@ public:
 
         vector<pcl::PointIndices> blob_clusters;
         // Select the correct color clouds from the segmentation
+        // TODO: this is a major bottleneck. improve performance
         for (int i = 0; i < clusters.size(); i++){
             pcl::PointIndices cluster = clusters[i];
 
             // Get a representative color in the cluster
 
             float n = cluster.indices.size();
-            //Hurr indexing through all these points is not so good
-            int r = 0; int g = 0; int b = 0;
+            float r = 0; float g = 0; float b = 0;
             for (int j = 0; j < n; j++){
                 pcl::PointRGB color = getCloudColorAt(cluster.indices[j]);
-                r += (int) color.r;
-                g += (int) color.g;
-                b += (int) color.b;
+                r += (float) color.r; //float typecasts suck
+                g += (float) color.g;
+                b += (float) color.b;
             }
             r /= n; g /= n; b /= n;
             cout << "Average color: " << r << ", " << g << ", " << b << endl;
-            pcl::PointRGB avg((unsigned char) r, (unsigned char) g, (unsigned char) b);
-            //avg = getCloudColorAt(cluster.indices[0]);
+            pcl::PointRGB avg((unsigned char) b, (unsigned char) g, (unsigned char) r);
 
             // Check if avg is within the clicked color
-            //TODO: get desired_color from click and radius from param
             if (isPointWithinDesiredRange(avg, desired_color, radius)){
                 blob_clusters.push_back(clusters[i]);
             }
@@ -218,47 +202,93 @@ public:
         object_poses = vector<geometry_msgs::Pose>();
         for (int i = 0; i < blob_clusters.size(); i++){
             // Get all points in the segmentation in cloud_iterator
-            const pcl::PointCloud<pcl::PointXYZRGB> cloud_subset = pcl::PointCloud<pcl::PointXYZRGB>(*cloud, blob_clusters[i].indices);
-            Eigen::Vector4f centroid;
+            const pcl::PointCloud<pcl::PointXYZRGB> cloud_subset =
+                pcl::PointCloud<pcl::PointXYZRGB>(*cloud, blob_clusters[i].indices);
+            //Eigen::Vector4f centroid;
+            Eigen::Vector3f centroid;
 
             //TODO: transform to base frame
 
-            //this centroid will be a BIT off because the shape is only 3 faces of a cube
-            pcl::compute3DCentroid(cloud_subset, centroid);
+            //get the moment of inertia
+            pcl::MomentOfInertiaEstimation<pcl::PointXYZRGB> inertia;
+            inertia.setInputCloud(cloud_subset);
+            inertia.compute();
+
+            //this centroid will be off because we get only 3 faces of a cube
+            inertia.getMassCenter(centroid);
             geometry_msgs::Point position;
-            position.x = centroid(0); position.y = centroid(1); position.z = centroid(2);
+            position.x = centroid(0);
+            position.y = centroid(1);
+            position.z = centroid(2);
 
-            //make this smart at some point
+            vector<float> eigenvals(3);
+            vector<Eigen::Vector3f> eigenvecs(3);
+            //Order is major, middle, minor
+            if(!inertia.getEigenVectors(eigenvals[0], eigenvals[1], eigenvals[2]) ||
+               !inertia.getEigenValues(eigenvecs[0], eigenvecs[1], eigenvecs[2]) ){
+                cout << "Couldn't get valid eigenvectors/values for moment of\
+                         inertia calculation" <<endl;
+                return;
+            }
+
+            const Eigen::Vector3f[3] basevecs = [ Eigen::Vector3f(1, 0, 0),
+                        Eigen::Vector3f(0, 1, 0 ), Eigen::Vector3f(0, 0, 1)];
+            //Get orientation from eigenvectors/values
+            /*float[3] angles;
+            Eigen::Matrix3f R = Matrix3d::Identity();
+            for (int j = 0; j < 3; j++){
+                Eigen::Vector3f p = eigenvecs[j];
+                Eigen::Vector3f q = basevecs[j];
+                //get angle between p and a base vec
+                angle = acos( p.normalized().dot( q.normalized() ) )
+                R = R.dot(Eigen::aa(angle, q));
+            }*/
+
+            Eigen::Quaternion q = Eigen::Quaternion.Identity();
+            for (int j = 0; j < 3; j++){
+                Eigen::Quaternion p;
+                q*=p.fromTwoVectors(basevecs[j], eigenvecs[j]);
+            }
+
             geometry_msgs::Quaternion orientation;
-            orientation.x = 0;
-            orientation.y = 0;
-            orientation.z = 0;
-            orientation.w = 1;
-            geometry_msgs::PoseStamped pose_in;
-            pose_in.pose.position = position; pose_in.pose.orientation = orientation;
-            pose_in.header.frame_id = "/camera_depth_optical_frame";
-            pose_in.header.stamp = ros::Time::now();
+            orientation.x = q.x;
+            orientation.y = q.y;
+            orientation.z = q.z;
+            orientation.w = q.w;
 
-            geometry_msgs::PoseStamped pose_out;
-            tf_listener.transformPose("/base", pose_in, pose_out);
-            //Now transform the pose to the base frame
-            object_poses.push_back(pose_out.pose);
-            
-            // TODO: Pose calculation for cube
-            // Get surface normals for cloud_subset
-            // Cluster into 3 surfaces
-            // Discard the one most parallel to Z, randomly choose the other
+            geometry_msgs::Pose pose;
+            pose.position = position; pose.orientation = orientation;
+            object_poses.push_back(pose);
+
         }
 
     }
 
     void callback(const sensor_msgs::PointCloud2::ConstPtr& msg){
+
+        //Transform the point cloud msg to the base frame
+
+        sensor_msgs::PointCloud2 msg_out;
+        tf_listener.transformPointCloud("/base", msg, msg_out);
+ 
+
         // Members: float x, y, z; uint32_t rgba
-        pcl::PointCloud <pcl::PointXYZRGB>::Ptr new_cloud (new pcl::PointCloud <pcl::PointXYZRGB>);
+        pcl::PointCloud <pcl::PointXYZRGB>::Ptr new_cloud
+                                    (new pcl::PointCloud <pcl::PointXYZRGB>);
 
         pcl::PCLPointCloud2 pcl_pc;
-        pcl_conversions::toPCL(*msg, pcl_pc);
+        pcl_conversions::toPCL(*msg_out, pcl_pc);
         pcl::fromPCLPointCloud2(pcl_pc, *new_cloud);
+        indices = pcl::IndicesPtr( new vector<int>() );
+        pcl::removeNaNFromPointCloud(*new_cloud, *new_cloud, *indices);
+
+        pcl::PassThrough<pcl::PointXYZRGB> pass;
+        pass.setInputCloud (new_cloud);
+        pass.setFilterFieldName ("z");
+        pass.setFilterLimits (filter_min, filter_max);
+        pass.filter (*indices);
+
+
         cloud = new_cloud;
         has_cloud = true;
     }
@@ -279,7 +309,7 @@ int main(int argc, char** argv){
         if(cs.hasColor()){
             cs.renderClusters();
         } else if(cs.hasCloud()){
-            cs.renderCloud(); //only do this once?
+            cs.renderCloud();
         }
         cs.publish_poses();
     }
